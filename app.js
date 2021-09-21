31fa1fe6bccb18e562d939f2ee48cb31b7aee36f
varan <varan2030@mail.ru>
2021-09-20 21:50:39 -0400

    added CORS

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ if(process.env.NODE_ENV === 'production'){
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
     app.use('/', express.static(path.join(__dirname, 'client', 'build' )));
-});
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
     })
